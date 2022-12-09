b11e9e2dcca982209d2b9dd60361cbcc51282e12
Anton Belov <anton.belov.al7@gmail.com>
2022-12-09 21:57:50 +0000

    frontend setup

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,3 +1,16 @@
+import Head from 'next/head'
+
 export default function HomePage() {
-    return <div>Welcome to Next.js!</div>
+    return (
+        <div>
+            <Head>
+                <title>GreenSt</title>
+            </Head>
+            <div className='flex justify-center items-center'>
+                <div className='bg-black'>
+                    Helo
+                </div>
+            </div>
+        </div>
+    )
 }
